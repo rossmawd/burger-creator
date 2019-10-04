13f96ec857796fec88b3a80499fb6f3096193e8b
Ross Mawdsley <rossmawd@gmail.com>
2019-10-04 18:13:42 +0100

    experimenting with React.createElement

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    //   return (
+    //     <div className="App">
+    // <h1>Let's get started!</h1> 
+    //     </div>
+    //   );
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'), 'Hi, I\'m a React App')
+  }
+}
+
+export default App;
