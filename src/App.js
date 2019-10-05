102e6c62728bf5238d45bf2759417c85b947b1fd
Ross Mawdsley <rossmawd@gmail.com>
2019-10-05 18:23:36 +0100

    covered props.children

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import Person from './Person/Person'
 
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    person: [
+      {name: "ross", age: 35},
+      {name: "Carol", age: 26}
+    
+    ]
+  }
   render() {
-    //   return (
-    //     <div className="App">
-    // <h1>Let's get started!</h1> 
-    //     </div>
-    //   );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'), 'Hi, I\'m a React App')
+      return (
+        <div className="App">
+    <h1>Let's get started!</h1> 
+    <Person {...this.state.person[0]}></Person>
+    <Person {...this.state.person[1]}></Person>
+   
+        </div>
+      );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'), 'Hi, I\'m a React App')
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+
+const Person = ({ name, age }) => {
+   
+  return (
+    <div>
+      <p>I/'m {name} and I am {age} years old!</p>
+      <p> </p>
+    </div>
+  )
+
+}
+
+export default Person;
